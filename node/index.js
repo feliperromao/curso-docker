25c098ce96345a4edc2937308e16310cde73d394
feliperromao <feliperomaocad@gmail.com>
2021-12-10 00:07:50 -0300

    conectando app node no mysql

--- node/index.js
+++ node/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const mysql = require('mysql')
 
 const app = express()
 const port = 3003
+
+const dbConfig = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const dbConnection = mysql.createConnection(dbConfig)
+
+const sqlQuery = `INSERT INTO people(name) values('Felipe')`
+
+dbConnection.query(sqlQuery)
+dbConnection.end()
 
 app.get('/', function (req, res) {
     return res.json({
